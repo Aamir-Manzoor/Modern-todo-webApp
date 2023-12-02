3ea7fb699ebda3af9cc7cee2b1e0aefa0257a4aa
aamir221 <aamirmanzoor372@gmail.com>
2023-12-02 16:51:44 +0530

    home

--- src/components/TasksSection/TaskItem/ActionsTaskItem.tsx
+++ src/components/TasksSection/TaskItem/ActionsTaskItem.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Task } from "../../../interfaces";
+import BtnEditTask from "./BtnEditTAsk";
+import BtnMarkAsImportant from "./BtnMarkAsImportant";
+import BtnDeleteTask from "./BtnDeleteTask";
+import BtnToggleCompleted from "./BtnToggleCompleted";
 
-const ActionsTaskItem: React.FC = () => {
+const ActionsTaskItem: React.FC<{ task: Task; isListInView1: boolean }> = ({
+  task,
+  isListInView1,
+}) => {
   return (
-    <div>
-     ActionsTaskItem
-    </div>
-  )
-}
+    <>
+      <div
+        className={`flex border-dashed border-slate-200 dark:border-slate-700/[.3] ${
+          isListInView1 ? "items-center" : "border-t-2 w-full pt-4 mt-4"
+        }`}
+      >
+        <BtnToggleCompleted
+          taskCompleted={task.completed}
+          taskId={task.id}
+          isListInView1={isListInView1}
+        />
+        <BtnMarkAsImportant taskId={task.id} taskImportant={task.important} />
+        <BtnDeleteTask taskId={task.id} />
+        <BtnEditTask task={task} />
+      </div>
+    </>
+  );
+};
 
 export default ActionsTaskItem;
