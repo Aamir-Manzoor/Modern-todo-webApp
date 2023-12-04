b1c14bb00e7cb006e5843b87d857bf5d3094fba3
aamir221 <aamirmanzoor372@gmail.com>
2023-12-04 21:48:41 +0530

    Header Tasks

---
+++ src/components/TasksSection/HeaderTasks.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BtnAddTask from '../Utilities/BtnAddTask';
+
+const HeaderTasks: React.FC = () => {
+  return (
+    <div>
+     HeaderTasks
+    </div>
+  )
+}
+
+export default HeaderTasks;
